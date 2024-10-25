5136c966be9fe5c8bfc1b4752fd7be47de5c778e
Lassi Palojärvi <Lassi.palojarvi@outlook.com>
2024-10-25 17:55:39 +0300

    Remove pnpm lock

---
+++ src/app/_components/navbar.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex items-center justify-between">
+        <Link href="/" className="text-xl font-bold text-white">
+          Momentum
+        </Link>
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/" className="text-white hover:text-gray-300">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="text-white hover:text-gray-300">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" className="text-white hover:text-gray-300">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
