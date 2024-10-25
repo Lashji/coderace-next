cf8665d29e3cc899146b9598ad5136827b5c6e2e
Lassi Palojärvi <Lassi.palojarvi@outlook.com>
2024-10-25 18:59:39 +0300

    Navbar logo

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,25 +1,39 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="bg-background p-4">
       <div className="container mx-auto flex items-center justify-between">
-        <Link href="/" className="text-xl font-bold text-white text-[#e06666]">
+        <Link
+          href="/"
+          className="justfy-center flex items-center gap-4 text-xl font-bold text-[#e06666] text-white"
+        >
+          <Image src="/logo.jpg" alt="Momentum Logo" width={32} height={32} />
           MOMENTUM
         </Link>
         <ul className="flex space-x-4">
           <li>
-            <Link href="/" className="text-white hover:text-gray-300 text-[#e06666]">
+            <Link
+              href="/"
+              className="text-[#e06666] text-white hover:text-gray-300"
+            >
               Home
             </Link>
           </li>
           <li>
-            <Link href="/about" className="text-white hover:text-gray-300 text-[#e06666]">
+            <Link
+              href="/about"
+              className="text-[#e06666] text-white hover:text-gray-300"
+            >
               About
             </Link>
           </li>
           <li>
-            <Link href="/contact" className="text-white hover:text-gray-300 text-[#e06666]">
+            <Link
+              href="/contact"
+              className="text-[#e06666] text-white hover:text-gray-300"
+            >
               Contact
             </Link>
           </li>
